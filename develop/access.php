207329361bb0321fd58deab71f022e0e9f76ed62
橋本真奈美 <manako.075h@gmail.com>
2023-10-20 21:50:47 +0900

    no message

---
+++ develop/access.php
@@ -0,0 +1,277 @@
+<!doctype html><?php
+ $page_settings = array(
+  "directory_pos" => "./",
+  "page_id" => "access",
+  "gnav_set" => "access",
+  "page_class" => "access",
+  "url" => "access.html",
+  "title" => "アクセス｜",
+  "keywords" => "アクセス,",
+  "description" => "アクセスページ。",
+  "add_stylesheet" => ["css/access.css"],
+  "add_script" =>  [],
+  "canonical_page_id" => "access", 
+  "logo_text" => "アクセス｜"
+);
+?>
+
+<?php require(INCLUDES."header.php"); ?>
+
+<main>
+	<!-- パンくずリスト -->
+  <div class="breadcrumb sp-hide">
+    <ul>
+      <li><a href="./index.html">シティテラス下丸子　トップ</a></li>
+      <li><p>アクセス</p></li>
+    </ul>
+  </div>
+	<!--// パンくずリスト -->
+
+  <header class="main-visual clearfix">
+    <!-- <h2 class="page-lead gothic">Hello New World.</h2> -->
+   <div class="image"><img src="./images/access/mv.jpg" alt="" class="pc"><img src="./images/access/mv-sp.jpg" alt="" class="sp"><p class="annotation white">航空写真</p></div>
+  </header><!-- main-visual -->
+
+  
+  <section class="intro sections clearfix">
+    <div class="inner">
+      <p class="tit">品川や渋谷など、都心主要エリアへ軽快なアクセス。</p>
+      <p class="lead">東急線各線を利用して、自由が丘などの人気のショッピングエリアや渋谷などの都心主要駅へスピーディーにアクセス。また、交通拠点である品川や国内外の主要都市をつなぐ羽田空港へも快適な移動が可能です。</p>
+      <div class="image img01">
+      <img src="./images/access/intro01.png" alt="">
+      <p class="annotation">路線図</p>
+      </div>
+      <ul class="column02">
+        <li>
+        <div class="image">
+          <img src="./images/access/intro02.svg" alt="">
+          </div>
+        </li>
+        <li>
+          <div class="image">
+          <img src="./images/access/intro03.svg" alt="">
+          </div>
+        </li>
+      </ul>
+      <ul class="column03">
+        <li>
+          <div class="image">
+          <img src="./images/access/sta01.jpg" alt="">
+          <p class="annotation white">image photo</p>
+          </div>
+          <div class="txt">
+          <img src="./images/access/sta01-txt.svg" alt="">
+          </div>
+        </li>
+        <li>
+          <div class="image">
+          <img src="./images/access/sta02.jpg" alt="">
+          <p class="annotation white">image photo</p>
+          </div>
+          <div class="txt">
+          <img src="./images/access/sta02-txt.svg" alt="">
+          </div>
+        </li>
+        <li>
+          <div class="image">
+          <img src="./images/access/sta03.jpg" alt="">
+          <p class="annotation white">image photo</p>
+          </div>
+          <div class="txt">
+          <img src="./images/access/sta03-txt.svg" alt="">
+          </div>
+        </li>
+        <li>
+          <div class="image">
+          <img src="./images/access/sta04.jpg" alt="">
+          <p class="annotation white">image photo</p>
+          </div>
+          <div class="txt">
+          <img src="./images/access/sta04-txt.svg" alt="">
+          </div>
+        </li>
+        <li>
+          <div class="image">
+          <img src="./images/access/sta05.jpg" alt="">
+          <p class="annotation white">image photo</p>
+          </div>
+          <div class="txt">
+          <img src="./images/access/sta05-txt.svg" alt="">
+          </div>
+        </li>
+        <li>
+          <div class="image">
+          <img src="./images/access/sta06.jpg" alt="">
+          <p class="annotation white">image photo</p>
+          </div>
+          <div class="txt">
+          <img src="./images/access/sta06-txt.svg" alt="">
+          </div>
+        </li>
+      </ul>
+    </div>
+  </section><!-- intro -->
+
+
+
+  <section class="sections network wrap clearfix">
+    <div class="bg">
+    <h3 class="text">1度の乗り換えで都内の人気エリアへ。<br>行動範囲が大きく広がる東急線ネットワーク。</h3>
+    <p class="lead">東急多摩川線は「多摩川」駅で東急東横線に乗り換えが可能。自由が丘や代官山、中目黒などの人気のショッピングエリアへスムーズにアクセスできます。さらに「渋谷」駅で東京メトロ副都心線に乗り入れて、新宿三丁目や池袋など多彩な都心主要駅をつないでいます。</p>
+    <div class="image map">
+    <img src="./images/access/network00.png" alt="">
+    <p class="annotation">路線図</p>
+    </div>
+    </div>
+    <ul class="clearfix">
+      <li>
+      <div class="image">
+      <img src="./images/access/network01.jpg" alt="">
+      <p class="annotation white">image photo</p>
+      </div>
+      </li>
+      <li>
+      <div class="image">
+      <img src="./images/access/network02.jpg" alt="">
+      <p class="annotation white">image photo</p>
+      </div>
+      </li>
+      <li>
+      <div class="image">
+      <img src="./images/access/network03.jpg" alt="">
+      <p class="annotation white">image photo</p>
+      </div>
+      </li>
+      <li>
+      <div class="image">
+      <img src="./images/access/network04.jpg" alt="">
+      <p class="annotation white">image photo</p>
+      </div>
+      </li>
+      <li>
+      <div class="image">
+      <img src="./images/access/network05.jpg" alt="">
+      <p class="annotation white">image photo</p>
+      </div>
+      </li>
+    </ul>
+  </section>
+
+
+
+  <section class="sections airport clearfix">
+    <div class="image">
+    <img src="./images/access/air01.jpg" alt="">
+    <p class="annotation white">image photo</p>
+    </div>
+    <div class="wrap">
+      <p class="text">多彩なルートで快適なエアポートアクセス。</p>
+      <p class="lead">「蒲田」駅でスムーズに乗り換えできる京浜急行空港線や、豊富な運行本数が便利なシャトルバスなど、移動手段を選べる自由が、旅行や出張を快適に。蒲田エリア近接だからこそ得られるアドバンテージです。</p>
+      <ul class="column02 bottom">
+        <li>
+          <div class="image">
+          <img src="./images/access/air02.jpg" alt="">
+          <p class="annotation white">image photo</p>
+          </div>
+        </li>
+        <li>
+          <div class="txt">
+          <img src="./images/access/air02-txt.svg" alt="">
+          </div>
+        </li>
+      </ul>
+      <ul class="column02">
+        <li>
+          <div class="image">
+          <img src="./images/access/air03.jpg" alt="">
+          <p class="annotation white">image photo</p>
+          </div>
+        </li>
+        <li>
+          <div class="txt">
+          <img src="./images/access/air03-txt.svg" alt="">
+          </div>
+        </li>
+      </ul>
+      <div class="check">
+        <ul class="column02">
+          <li>
+            <div class="txt">
+            <img src="./images/access/air04-txt.svg" alt="">
+            </div>
+          </li>
+          <li>
+            <div class="image">
+            <img src="./images/access/air04.jpg" alt="">
+            <p class="annotation white">image photo</p>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </section>
+
+
+  <section class="sections car clearfix">
+    <div class="image">
+    <img src="./images/access/car01.jpg" alt="">
+    <p class="annotation white">image photo</p>
+    </div>
+    <div class="wrap">
+      <p class="text">主要高速道路を駆使して全国に広がる快適ドライブ。</p>
+      <p class="lead">主要な高速道路へのアクセスがスムーズな「シティテラス下丸子」。都市へ、郊外へ、カーライフの楽しみも広がります。</p>
+      <ul class="column02">
+        <li>
+          <div class="image">
+          <img src="./images/access/car02.png" alt="">
+          <p class="annotation">カーアクセス図</p>
+          </div>
+          <p class="mini">※1.東京外環自動車道（東京区間）についての情報は「国土交通省関東地方整備局」ホームページを基にしています。（2023年7月現在）</p>
+        </li>
+        <li>
+          <div class="txt">
+          <img src="./images/access/car02-txt.svg" alt="">
+          </div>
+        </li>
+      </ul>
+      <p class="tit01">首都高速1号羽田線<br class="sp"><span>「羽田」</span>入口から</p>
+      <ul class="column03">
+        <li>
+          <div class="image">
+          <img src="./images/access/car03.jpg" alt="">
+          <p class="annotation white">image photo</p>
+          </div>
+          <div class="txt">
+          <img src="./images/access/car03-txt.svg" alt="">
+          </div>
+        </li>
+        <li>
+          <div class="image">
+          <img src="./images/access/car04.jpg" alt="">
+          <p class="annotation white">image photo</p>
+          </div>
+          <div class="txt">
+          <img src="./images/access/car04-txt.svg" alt="">
+          </div>
+        </li>
+        <li>
+          <div class="image">
+          <img src="./images/access/car05.jpg" alt="">
+          <p class="annotation white">image photo</p>
+          </div>
+          <div class="txt">
+          <img src="./images/access/car05-txt.svg" alt="">
+          </div>
+        </li>
+      </ul>
+    </div>
+  </section>
+
+
+<div class="annotation-area">
+<p class="page-annotation js-scroll wrap clearfix">※掲載の電車所要時間は待ち時間・乗り換え時間を含んだ通勤時（カッコ内は平常時）の目安であり、時間帯により多少異なります。<br>※掲載の路線図は、一部路線・駅等を抜粋して表記しています。<br>※掲載の距離表示については地図上の概測距離を、徒歩分数表示については80ｍを1分として算出し、端数を切り上げたものです。<br>※掲載のバスの所要時間は目安であり、時間帯により多少異なります。出典：東急バス・京急バスホームページ（2023年6月現在）。<br>※掲載のカーアクセス図は、一部、道路・出入口・IC等を抜粋して表記しています。※掲載の所要時間は、時間帯・交通事情により異なる場合があります。<br>※掲載の高速道路距離表示については地図上の概測距離を、「ドラぷら（NEXCO東日本）」ホームページにて検索（2023年2月現在）したものです。但し、天候不順、周辺の交通状況（朝・夕の混雑、交通事故等）等により大幅な遅延が生じる場合があります。<br>※タクシー料金は、初乗り500円の普通車（中型車）を日中時（深夜時は22時～5時）に一般道で利用した場合の参考例です。タクシー会社・走行ルートおよび交通状況によって料金は異なります。※タクシーサイトより（2023年2月調べ）<br>※掲載の情報は2023年7月現在のものです。</p></div>
+</main>
+
+<?php require(INCLUDES."footer.php"); ?>
+</body>
+</html>
