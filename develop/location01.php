27d57955ec4afcf3bdb0e248eacd549dc7cd739d
橋本真奈美 <manako.075h@gmail.com>
2023-10-20 23:52:27 +0900

    no message

---
+++ develop/location01.php
@@ -0,0 +1,266 @@
+<!doctype html><?php
+ $page_settings = array(
+  "directory_pos" => "./",
+  "page_id" => "location",
+  "gnav_set" => "location",
+  "page_class" => "location01",
+  "url" => "location01.html",
+  "title" => "さまざまな都市機能に近接する「下丸子」",
+  "keywords" => "さまざまな都市機能に近接する「下丸子」,",
+  "description" => "さまざまな都市機能に近接する「下丸子」ページ。",
+  "add_stylesheet" => ["css/location.css","css/location01.css"],
+  "add_script" =>  ["js/location.js"],
+  "canonical_page_id" => "location", 
+  "logo_text" => "さまざまな都市機能に近接する「下丸子」｜"
+);
+?>
+
+<?php require(INCLUDES."header.php"); ?>
+
+<main>
+	<!-- パンくずリスト -->
+  <div class="breadcrumb sp-hide">
+    <ul>
+      <li><a href="./index.html">シティテラス下丸子　トップ</a></li>
+      <li><a href="./location01.html">ロケーション</a></li>
+      <li><p>さまざまな都市機能に近接する「下丸子」</p></li>
+    </ul>
+  </div>
+	<!--// パンくずリスト -->
+
+  <div class="location-link location-link--3">
+    <a href="location01.html" class="current"><p>さまざまな都市機能に近接する「下丸子」</p></a>
+    <a href="location02.html"><p>豊かな自然の潤いを身近に</p></a>
+    <a href="location03.html"><p>毎日を支える充実の利便施設</p></a>
+  </div>
+
+  <header class="main-visual clearfix">
+    <!-- <h2 class="page-lead gothic">Hello New World.</h2> -->
+   <div class="image"><img src="./images/location01/mv.jpg" alt="" class="pc"><img src="./images/location01/mv-sp.jpg" alt="" class="sp"><p class="annotation white">image photo</p></div>
+  </header><!-- main-visual -->
+
+
+  <section class="intro sections clearfix">
+    <div class="inner">
+      <p class="tit">品川・羽田へ軽快。多彩なエリアが身近に。</p>
+      <p class="lead">国内外への広域アクセスに便利な交通拠点、品川・羽田を快適に利用できる「シティテラス下丸子」。休日のショッピングやレジャースポットとして人気の自由が丘や二子玉川をはじめ、魅力あふれる多彩なエリアも身近なポジションです。</p>
+      <div class="image img01">
+        <img src="./images/location01/intro01.png" alt="" class="pc">
+        <p class="annotation">広域エリア概念図</p>
+      </div>
+      <p class="mini">※1.高輪ゲートウェイシティ〈仮称〉（2025年度開業予定）　出典：東日本旅客鉄道株式会社ニュースリリース（2022年4月21日付）※2.リニア中央新幹線（品川・名古屋間：2027年開業予定、品川・大阪間：2045年開業予定※一部区間の工事遅延により遅れる見通しがあります。） 出典：国土交通省ホームぺージ、JR東海ホームぺージ（2023年6月現在）※3.羽田空港アクセス線〈仮称〉（2031年度開業予定）　出典：東日本旅客鉄道株式会社ニュースリリース（2023年4月4日付）</p>
+    </div>
+  </section><!-- intro -->
+
+
+
+  <section class="sections areas area01 clearfix">
+    <div class="image img00">
+      <img src="./images/location01/area01-00.jpg">
+      <p class="annotation white">image photo</p>
+    </div>
+    <div class="inner">
+      <p class="caps bottom">※「羽田空港第3ターミナル」駅まで?「下丸子」駅より東急多摩川線利用、「蒲田」駅／「京急蒲田」駅で京急空港線特急に乗り換え</p>
+      <p class="text">世界の玄関口として進化を続ける「羽田空港」。<br>新たな賑わい拠点も次々と誕生。</p>
+      <p class="lead">日本と世界を結ぶ空の玄関口としての役割が一段と高まっている羽田空港。近年は、多彩なショップやレストラン、ホテルなどを備えた「羽田エアポートガーデン」や「羽田イノベーションシティ」のオープンによって、フライト利用以外の新たな楽しみのために多くの人々が訪れています。</p>
+      <div class="box box01">
+        <p class="name">羽田エアポートガーデン<span>（約10.5km）</span></p>
+        <ul class="column02 bottom">
+          <li>
+            <div class="image">
+              <img src="./images/location01/area01-01.jpg">
+              <p class="annotation white">image photo</p>
+            </div>
+          </li>
+          <li>
+            <p class="info">「羽田空港第3ターミナル」駅に直結する大型複合施設が2023年1月にオープン。ホテル、ショッピングゾーン、天然温泉、イベントホールなどの施設が集結。また、日本各地を結ぶバスターミナルを備え、空港利用の際に気軽に立ち寄れる賑わいのスポットです。</p>
+            <div class="image">
+              <img src="./images/location01/area01-02.jpg">
+              <p class="annotation white">image photo</p>
+            </div>
+          </li>
+        </ul>
+        <ul class="column02">
+          <li>
+            <div class="image">
+              <img src="./images/location01/area01-03.jpg">
+              <p class="annotation white">image photo</p>
+            </div>
+            <p class="txt01">■ 約80店舗のショッピングゾーン</p>
+            <p class="txt02">日本の名産品や旅行グッズをはじめ、ファッション、雑貨、グルメなど多彩なショップが集結。</p>
+          </li>
+          <li>
+            <div class="image">
+              <img src="./images/location01/area01-04.jpg">
+              <p class="annotation white">image photo</p>
+            </div>
+            <p class="txt01">■ 展望天然温泉 泉天空の湯</p>
+            <p class="txt02">飛び交う飛行機や富士山を眺めがら、心地よく湯船に浸かれる展望天然温泉。</p>
+          </li>
+        </ul>
+      </div>
+      <div class="box box02">
+        <ul class="column02 bottom">
+          <li>
+            <p class="name">羽田イノベーションシティ<span>（約8.4km）</span></p>
+            <p class="info">「羽田空港第3ターミナル」駅から1駅の「天空橋」駅に直結する、敷地面積約5.6ヘクタールの大型複合施設。ショッピングやグルメに加えて、日本文化やライブイベントといった、訪れる人に新しい体験を届ける施設が充実しています。新たなすごし方=イノベーションを提供する羽田の新たな街です。</p>
+          </li>
+          <li>
+            <div class="image">
+              <img src="./images/location01/area01-05.jpg">
+              <p class="annotation white">image photo</p>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div><!-- inner -->
+  </section><!-- areas area01 -->
+
+
+
+  <section class="sections areas area02 clearfix">
+    <div class="image img00">
+      <img src="./images/location01/area02-00.jpg">
+      <p class="annotation white">image photo</p>
+    </div>
+    <div class="inner">
+      <p class="caps bottom">※「品川」駅まで?「下丸子」駅より東急多摩川線利用、「蒲田」駅でJR京浜東北線（平常時：JR京浜東北線快速）に乗り換え</p>
+      <p class="text">都内屈指のターミナル駅「品川」が、<br class="pc">未来に向けさらなる進化を遂げる。</p>
+      <p class="lead">東京都が推進する外国企業誘致プロジェクト「アジアヘッドクォーター特区」として、グローバル化をめざしている品川エリア。開業を控える「リニア中央新幹線」の始発駅であり、「羽田空港」への連絡経路である「品川」駅は、国際交流の重要な拠点としてもさらなる発展が期待されています。</p>
+      <div class="box">
+        <ul class="column02">
+          <li>
+            <p class="name">リニア中央新幹線の開業<span>（予定）</span>で、<br class="pc">さらに利便性が増す「品川」駅。</p>
+            <p class="info">多数の在来線やJR東海道新幹線が乗り入れる交通の要衝「品川」駅に、時速約500kmという画期的なスピードで、品川・名古屋間を最速約40分、品川・大阪間を最速約67分で結ぶリニア中央新幹線が開業予定。全線開通後は、時短メリットによる通勤、旅行エリアの拡大、ライフスタイルの変化など、イノベーションが期待されます。</p>
+            <ul class="column02 imgs">
+              <li>
+                <div class="image">
+                  <img src="./images/location01/area02-01.jpg">
+                  <p class="annotation white">image photo</p>
+                </div>
+              </li>
+              <li>
+                <div class="image">
+                  <img src="./images/location01/area02-02.jpg">
+                  <p class="annotation white">image photo</p>
+                </div>
+              </li>
+            </ul>
+          </li>
+          <li>
+            <div class="image">
+              <img src="./images/location01/area02-03.svg">
+            </div>
+          </li>
+        </ul>
+      </div>
+      </div><!-- inner -->
+  </section><!-- areas area02 -->
+
+
+
+  <section class="sections areas area03 clearfix">
+    <div class="image img00">
+      <img src="./images/location01/area03-00.jpg">
+      <p class="annotation white">image photo</p>
+    </div>
+    <div class="inner">
+      <p class="caps bottom">※「蒲田」駅まで?「下丸子」駅より東急多摩川線利用（直通）</p>
+      <p class="text">駅直結の大型商業が便利な「蒲田」駅。<br>駅前には個性豊かな商店街も。</p>
+      <p class="lead">JR「蒲田」駅に直結する2つの大型商業施設に加えて、駅前には多くの店舗が連なり、ショッピングやカルチャー、グルメまで多彩に揃う蒲田の街。乗り換え時などに気軽に立ち寄れるスポットとして、暮らしの利便性を高めます。</p>
+      <ul class="column02">
+        <li>
+          <div class="image">
+            <img src="./images/location01/area03-01.jpg">
+          </div>
+          <div class="txt">
+            <img src="./images/location01/area03-01-txt.svg">
+          </div>
+        </li>
+        <li>
+          <div class="image">
+            <img src="./images/location01/area03-02.jpg">
+          </div>
+          <div class="txt">
+            <img src="./images/location01/area03-02-txt.svg">
+          </div>
+        </li>
+      </ul>
+      <div class="box">
+        <ul class="column02">
+          <li>
+            <p class="name">人々を心地よく迎え<br class="pc">豊かな賑わいをもたらす蒲田。</p>
+            <p class="info">「蒲田」駅周辺では、羽田空港の国際化や交通環境の変化などに対応し、環境配慮も含めたまちづくりを目指す「蒲田駅周辺再編プロジェクト」が進行中。羽田空港の国際線の増便や近隣都市の再開発計画によって、観光客や来訪者による駅利用者の増加が見込まれる中、利便性の高い、環境にやさしい駅前空間の実現を推進しています。</p>
+            <p class="mini">※蒲田駅周辺再編プロジェクト（2023年12月策定）<br>出典：大田区ホームページ（2023年6月現在）</</p>
+          </li>
+          <li>
+            <div class="image">
+              <img src="./images/location01/area03-03.png">
+            </div>
+          </li>
+        </ul>
+      </div>
+      </div><!-- inner -->
+  </section><!-- areas area03 -->
+
+
+
+  <section class="sections areas area04 clearfix">
+    <div class="image img00">
+      <img src="./images/location01/area04-00.jpg">
+      <p class="annotation white">image photo</p>
+    </div>
+    <div class="inner">
+      <p class="caps bottom">※「自由が丘」駅まで?「鵜の木」駅より東急多摩川線利用、「多摩川」駅で東急東横線に乗り換え</p>
+      <p class="text">「自由が丘」へも気軽にお出かけ。<br>この街を休日の散策エリアにする暮らし。</p>
+      <p class="lead">洗練された街並みに、個性が集う街、自由が丘。駅前を東西に走るマリ・クレール通りをはじめ、学園通りやカトレア通りなどに店舗が点在。数々のストリートにファッショナブルでおしゃれなショップやレストランが並ぶ人気の街が、日々の暮らしを華やかに彩ります。</p>
+      <div class="other">
+        <p class="tit01">OTHER AREA</p>
+        <ul class="column03">
+          <li>
+            <div class="image">
+            <img src="./images/location01/area04-01.jpg">
+            <p class="annotation white">image photo</p>
+            </div>
+            <div class="txt">
+              <p class="info">「二子玉川ライズ」（約7.2km）や「多摩川高島屋ショッピングセンター」（約8.0km）、駅周辺に点在するカフェやレストランで、ショッピングやグルメが楽しめます。</p>
+            </div>
+          </li>
+          <li>
+            <div class="image">
+            <img src="./images/location01/area04-02.jpg">
+            <p class="annotation white">image photo</p>
+            </div>
+            <div class="txt">
+              <p class="info">駅前には、約160の専門店が集まる「グランツリー武蔵小杉」（約3.9km）や「コスギサードアヴェニュー」（約4.4km）など大型商業施設が充実。</p>
+            </div>
+          </li>
+          <li>
+            <div class="image">
+            <img src="./images/location01/area04-03.jpg">
+            <p class="annotation white">image photo</p>
+            </div>
+            <div class="txt">
+              <p class="info">「ラゾーナ川崎プラザ」（約5.8km）などの開発によって人気エリアとなった川崎。「東芝未来科学館」（約5.7km）など親子で楽しめるレジャースポットも揃っています。</p>
+            </div>
+          </li>
+        </ul>
+        <p class="mini">※1.「二子玉川」駅まで?「鵜の木」駅より東急多摩川線利用、「多摩川」駅で東急東横線急行に乗り換え、「自由が丘」駅で東急大井町線急行（平常時：東急大井町線）に乗り換え※2.「武蔵小杉」駅まで?「鵜の木」駅より東急多摩川線利用、「多摩川」駅で東急東横線急行に乗り換え※3.「川崎」駅まで?「下丸子」駅より東急多摩川線利用、「蒲田」駅でJR京浜東北線に乗り換え</p>
+      </div><!-- inner -->
+  </section><!-- areas area04 -->
+
+
+
+<div class="annotation-area">
+<p class="page-annotation js-scroll wrap clearfix">※掲載の広域エリア概念図は一般道路、線路、施設等を抜粋して表記しています。<br>※掲載の電車所要時間は待ち時間・乗り換え時間を含んだ通勤時（カッコ内は平常時）の目安であり、時間帯により多少異なります。<br>※掲載の距離表示は地図上の概測距離です。<br>※掲載の環境写真は2023年7月に撮影。<br>※店舗・施設の内容等は将来変わる場合があります。<br>※掲載の情報は2023年7月現在のものです。</p></div>
+</main>
+
+  <div class="location-link location-link--3">
+    <a href="location01.html" class="current"><p>さまざまな都市機能に近接する「下丸子」</p></a>
+    <a href="location02.html"><p>豊かな自然の潤いを身近に</p></a>
+    <a href="location03.html"><p>毎日を支える充実の利便施設</p></a>
+  </div>
+
+<?php require(INCLUDES."footer.php"); ?>
+</body>
+</html>
