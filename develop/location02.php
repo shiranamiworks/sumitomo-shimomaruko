313e670460ea7e239f8ea8c24f75f84e8c5690d9
橋本真奈美 <manako.075h@gmail.com>
2023-10-23 17:36:36 +0900

    no message

---
+++ develop/location02.php
@@ -0,0 +1,277 @@
+<!doctype html><?php
+ $page_settings = array(
+  "directory_pos" => "./",
+  "page_id" => "location",
+  "gnav_set" => "location",
+  "page_class" => "location02",
+  "url" => "location02.html",
+  "title" => "豊かな自然の潤いを身近に",
+  "keywords" => "豊かな自然の潤いを身近に,",
+  "description" => "豊かな自然の潤いを身近にページ。",
+  "add_stylesheet" => ["css/location.css","css/location02.css"],
+  "add_script" =>  ["js/location.js"],
+  "canonical_page_id" => "location", 
+  "logo_text" => "豊かな自然の潤いを身近に｜"
+);
+?>
+
+<?php require(INCLUDES."header.php"); ?>
+
+<main>
+	<!-- パンくずリスト -->
+  <div class="breadcrumb sp-hide">
+    <ul>
+      <li><a href="./index.html">シティテラス下丸子　トップ</a></li>
+      <li><a href="./location01.html">ロケーション</a></li>
+      <li><p>豊かな自然の潤いを身近に</p></li>
+    </ul>
+  </div>
+	<!--// パンくずリスト -->
+
+  <div class="location-link location-link--3">
+    <a href="location01.html"><p>さまざまな都市機能に近接する「下丸子」</p></a>
+    <a href="location02.html" class="current"><p>豊かな自然の潤いを身近に</p></a>
+    <a href="location03.html"><p>毎日を支える充実の利便施設</p></a>
+  </div>
+
+  <header class="main-visual clearfix">
+    <!-- <h2 class="page-lead gothic">Hello New World.</h2> -->
+   <div class="image"><img src="./images/location02/mv.jpg" alt="" class="pc"><img src="./images/location02/mv-sp.jpg" alt="" class="sp"><p class="annotation white">多摩川ガス橋緑地（約380m／徒歩5分）</p></div>
+  </header><!-- main-visual -->
+
+
+  <section class="intro sections clearfix">
+    <div class="inner">
+      <p class="tit">あふれる自然を感じながら、毎日を心地よく暮らす。</p>
+    </div>
+  </section><!-- intro -->
+
+
+
+  <section class="sections areas area01 clearfix">
+    <div class="image img00">
+      <img src="./images/location02/area01-00.jpg">
+      <p class="annotation white">多摩川ガス橋緑地（約380m／徒歩5分）</p>
+    </div>
+    <div class="inner">
+      <p class="text">スポーツや散策を楽しむ人で賑わう、<br class="pc">緑豊かな多摩川河川敷。</p>
+      <p class="lead">「シティテラス下丸子」のすぐ傍に広がる多摩川河川敷の「多摩川ガス橋緑地」には、サイクリングロードや野球場、テニスコートなどが整備されています。天気のいい日には、散歩やジョギング、ピクニックを楽しむ地域の人々で賑わっています。</p>
+      <ul class="column02">
+        <li>
+          <div class="image">
+          <img src="./images/location02/area01-01.jpg">
+          </div>
+        </li>
+        <li>
+          <div class="image bottom">
+          <img src="./images/location02/area01-02.jpg">
+          </div>
+          <div class="image">
+          <img src="./images/location02/area01-03.jpg">
+          </div>
+        </li>
+      </ul>
+      <p class="caps">多摩川ガス橋緑地（約380m／徒歩5分）</p>
+      <p class="txt01">120本を超える桜並木がつくる春の風景。</p>
+      <p class="txt02">花見の人気スポットでもある多摩川河川敷の「多摩川ガス橋緑地」は、毎年開花を心待ちにしていた人々で賑わいます。大田区が取り組んだ植樹事業によって植えられた桜の数は約120本。見頃の時期には春色に染まる多摩川沿いを楽しめます。</p>
+      <div class="image img04">
+        <img src="./images/location02/area01-04.jpg">
+        <p class="annotation">多摩川ガス橋緑地（約380m／徒歩5分）</p>
+      </div>
+        <ul class="column02">
+          <li>
+            <p class="txt01">打ち上げ花火が彩る多摩川の夏。</p>
+            <p class="txt02 bottom">昭和62年に、平和を祈る想いからスタートした「花火の祭典」。多摩川の河川敷から打ち上げられる約4,000発の花火は、大田区の一大イベントです。毎年の夏の思い出に夜空に咲く大輪の華を、家族みんなで観るしあわせが、この場所にあります。</p>
+            <p class="mini">出典：大田区ホームページ 大田区平和都市宣言記念事業「花火の祭典」（2023年6月現在）<br>※開催内容は変更・中止になる場合があります。</p>
+          </li>
+          <li>
+          <div class="image">
+              <img src="./images/location02/area01-05.jpg">
+              <p class="annotation white">image photo</p>
+            </div>
+          </li>
+        </ul>
+    </div><!-- inner -->
+  </section><!-- areas area01 -->
+
+
+
+  <section class="sections areas area02 clearfix">
+    <div class="inner">
+      <p class="text">子どもたちがのびのびと遊べる、<br class="pc">さまざまな公園が徒歩5分圏内に6つ。</p>
+      <div class="image img01">
+        <img src="./images/location02/area02-01.jpg">
+        <p class="annotation white">image photo</p>
+      </div>
+      <ul class="column02">
+        <li>
+          <div class="image">
+          <img src="./images/location02/area02-02.jpg">
+          </div>
+        </li>
+        <li>
+          <div class="image">
+          <img src="./images/location02/area02-03.jpg">
+          </div>
+        </li>
+      </ul>
+      <p class="txt01">下丸子公園<span>（約230m／徒歩3分）</span></p>
+      <p class="txt02 bottom">昭和62年に、平和を祈る想いからスタートした「花火の祭典」。多摩川の河川敷から打ち上げられる約4,000発の花火は、大田区の一大イベントです。毎年の夏の思い出に夜空に咲く大輪の華を、家族みんなで観るしあわせが、この場所にあります。</p>
+      <ul class="column02">
+        <li>
+          <div class="image">
+          <img src="./images/location02/area02-04.jpg">
+          </div>
+          <p class="txt01">くすのき公園<span>（約90m／徒歩2分）</span></p>
+        </li>
+        <li>
+          <div class="image">
+          <img src="./images/location02/area02-05.jpg">
+          </div>
+          <p class="txt01">下丸子余情公園<span>（約210m／徒歩3分）</span></p>
+        </li>
+      </ul>
+      <ul class="column03">
+        <li>
+          <div class="image">
+          <img src="./images/location02/area02-06.jpg">
+          </div>
+          <p class="txt01">鵜の木二丁目児童公園<br><span>（約220m／徒歩3分）</span></p>
+        </li>
+        <li>
+          <div class="image">
+          <img src="./images/location02/area02-07.jpg">
+          </div>
+          <p class="txt01">あおぞら児童公園<br><span>（約350m／徒歩5分）</span></p>
+        </li>
+        <li>
+          <div class="image">
+          <img src="./images/location02/area02-08.jpg">
+          </div>
+          <p class="txt01">あけぼの児童公園<br><span>（約370m／徒歩5分）</span></p>
+        </li>
+      </ul>
+      </div><!-- inner -->
+  </section><!-- areas area02 -->
+
+
+
+  <section class="sections areas area03 clearfix">
+    <div class="image img00">
+      <img src="./images/location02/area03-00.jpg">
+      <p class="annotation white">image photo</p>
+    </div>
+    <div class="inner">
+      <p class="text">子どもの成長をしっかりサポートする<br class="pc">充実した保育・教育環境。</p>
+      <p class="lead">身近に数多くの幼稚園・保育園が点在する「シティテラス下丸子」。小学校・中学校も通いやすい距離にあり、良好な子育て環境が整っています。</p>
+      <p class="line">幼稚園・保育園は徒歩10分圏内に<span>16</span>園</p>
+      <ul class="column03">
+        <li>
+          <div class="image">
+            <img src="./images/location02/area03-01.jpg">
+          </div>
+          <p class="name">キッズラボ下丸子園</p>
+          <div class="txt">
+            <img src="./images/location02/area03-01-txt.svg">
+          </div>
+        </li>
+        <li>
+          <div class="image">
+            <img src="./images/location02/area03-02.jpg">
+          </div>
+          <p class="name">グローバルキッズ鵜の木園</p>
+          <div class="txt">
+            <img src="./images/location02/area03-02-txt.svg">
+          </div>
+        </li>
+        <li>
+          <div class="image">
+            <img src="./images/location02/area03-03.jpg">
+          </div>
+          <p class="name">嶺町幼稚園</p>
+          <div class="txt">
+            <img src="./images/location02/area03-03-txt.svg">
+          </div>
+        </li>
+      </ul>
+      <p class="line line02">広い施設でのびのびと学べる通学校</p>
+      <ul class="column02">
+        <li>
+          <div class="image">
+            <img src="./images/location02/area03-04.jpg">
+          </div>
+          <p class="name">大田区立嶺町小学校（約870m／徒歩11分）</p>
+        </li>
+        <li>
+          <div class="image">
+            <img src="./images/location02/area03-05.jpg">
+          </div>
+          <p class="name">大田区立矢口中学校（約970m／徒歩13分）</p>
+        </li>
+      </ul>
+      </div><!-- inner -->
+  </section><!-- areas area03 -->
+
+
+
+  <section class="sections areas area04 clearfix">
+    <div class="inner">
+      <p class="point">TOPICS</p>
+      <p class="tit">住み続けたい街「大田区」は、子育てにも優しい街。</p>
+      <ul class="column02">
+        <li>
+          <p class="lead">2021年に実施した「大田区政に関する世論調査」によると、「あなたは大田区に住み続けたいと思いますか?」の質問に対して、半数以上の人が「ずっと住み続けたい」と回答し、「当分は住み続けたい」と回答した人を合わせると8割以上となる結果に。区民の定住意向の高さから、大田区住まいの魅力が伝わってきます。</p>
+        </li>
+        <li>
+          <div class="image">
+          <img src="./images/location02/area04-01.png">
+          <p class="annotation">出典：大田区政に関する世論調査（2021年7月実施）</p>
+          </div>
+        </li>
+      </ul>
+      <div class="image tit-img">
+        <img src="./images/location02/tit01.png">
+      </div>
+      <ul class="column02 list02">
+      <li>
+          <div class="image">
+          <img src="./images/location02/area04-02.png">
+          <p class="annotation">出典：東京都建設局ホームページ「東京都都市公園等区市町村別面積：人口比率表（2022年4月版）」</p>
+          </div>
+        </li>
+        <li>
+          <p class="lead">平成23年から緑地の保全や都市公園の整備を進めてきた大田区。その結果、東京23区内で都市公園数が最も多い区になりました。「シティテラス下丸子」のまわりにも多くの公園があり、子どもから大人まで公園でのひとときを楽しんでいます。</p>
+        </li>
+      </ul>
+      <div class="image tit-img">
+        <img src="./images/location02/tit02.png">
+      </div>
+      <ul class="column02 list03">
+        <li>
+          <p class="lead">大田区は、私立保育園の開設支援を積極的に行った結果、2021年と2022年で待機児童ゼロを実現しました。さらに区立保育園の民営化による保育サービスの充実や、保育園園長をメンバーとしたプロジェクトチームの発足など、区全体で保育水準の向上にも取り組んでいます。</p>
+        </li>
+        <li>
+          <div class="image">
+          <img src="./images/location02/area04-03.png">
+          <p class="annotation">出典：大田区ホームページ 「保育所等利用待機児童の状況」（2022年4月1日現在）</p>
+          </div>
+        </li>
+      </ul>
+    </div><!-- inner -->
+  </section><!-- areas area04 -->
+
+
+
+<div class="annotation-area">
+<p class="page-annotation js-scroll wrap clearfix">※掲載の距離表示については地図上の概測距離を、徒歩分数表示については80mを1分として算出し、端数を切り上げたものです。<br>※掲載の環境写真は2022年4月?2023年7月に撮影。<br>※施設の内容等は将来変わる場合があります。<br>※掲載の情報は2023年7月現在のものです。</p></div>
+</main>
+
+  <div class="location-link location-link--3">
+    <a href="location01.html"><p>さまざまな都市機能に近接する「下丸子」</p></a>
+    <a href="location02.html" class="current"><p>豊かな自然の潤いを身近に</p></a>
+    <a href="location03.html"><p>毎日を支える充実の利便施設</p></a>
+  </div>
+
+<?php require(INCLUDES."footer.php"); ?>
+</body>
+</html>
