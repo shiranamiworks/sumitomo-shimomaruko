11ae06d3323e7332638702b44e0339460f518a0b
橋本真奈美 <manako.075h@gmail.com>
2023-10-23 18:34:46 +0900

    no message

---
+++ develop/location03.php
@@ -0,0 +1,182 @@
+<!doctype html><?php
+ $page_settings = array(
+  "directory_pos" => "./",
+  "page_id" => "location",
+  "gnav_set" => "location",
+  "page_class" => "location03",
+  "url" => "location03.html",
+  "title" => "毎日を支える充実の利便施設",
+  "keywords" => "毎日を支える充実の利便施設,",
+  "description" => "毎日を支える充実の利便施設ページ。",
+  "add_stylesheet" => ["css/location.css","css/location03.css"],
+  "add_script" =>  ["js/location.js"],
+  "canonical_page_id" => "location", 
+  "logo_text" => "毎日を支える充実の利便施設｜"
+);
+?>
+
+<?php require(INCLUDES."header.php"); ?>
+
+<main>
+	<!-- パンくずリスト -->
+  <div class="breadcrumb sp-hide">
+    <ul>
+      <li><a href="./index.html">シティテラス下丸子　トップ</a></li>
+      <li><a href="./location01.html">ロケーション</a></li>
+      <li><p>毎日を支える充実の利便施設</p></li>
+    </ul>
+  </div>
+	<!--// パンくずリスト -->
+
+  <div class="location-link location-link--3">
+    <a href="location01.html"><p>さまざまな都市機能に近接する「下丸子」</p></a>
+    <a href="location02.html"><p>豊かな自然の潤いを身近に</p></a>
+    <a href="location03.html" class="current"><p>毎日を支える充実の利便施設</p></a>
+  </div>
+
+  <header class="main-visual clearfix">
+    <!-- <h2 class="page-lead gothic">Hello New World.</h2> -->
+   <div class="image"><img src="./images/location03/mv.jpg" alt="" class="pc"><img src="./images/location03/mv-sp.jpg" alt="" class="sp"><p class="annotation white">image photo</p></div>
+  </header><!-- main-visual -->
+
+
+  <section class="intro sections clearfix">
+    <div class="inner">
+      <p class="tit">毎日の買い物に便利なスーパーや個性豊かなショップ。</p>
+    </div>
+  </section><!-- intro -->
+
+
+
+  <section class="sections areas area01 clearfix">
+    <div class="image img00">
+      <img src="./images/location03/area01-00.jpg">
+      <p class="annotation white">image photo</p>
+    </div>
+    <div class="inner">
+      <p class="text">徒歩1分の大型スーパーマーケットなど、<br class="pc">生活を支える商業施設が多数。</p>
+      <p class="lead">「シティテラス下丸子」から徒歩1分の「オリンピック下丸子店」や、24時間営業の「西友下丸子店」など、近隣に点在する多数のスーパーが日々の暮らしにゆとりをもたらします。</p>
+      <ul class="column02 bottom">
+        <li>
+          <div class="image">
+          <img src="./images/location03/area01-01.jpg">
+          </div>
+        </li>
+        <li>
+          <div class="image">
+          <img src="./images/location03/area01-01-txt.svg">
+          </div>
+        </li>
+      </ul>
+      <ul class="column02 bottom">
+        <li>
+          <div class="image">
+          <img src="./images/location03/area01-02.jpg">
+          </div>
+          <div class="txt info">
+          <img src="./images/location03/area01-02-txt.svg">
+          </div>
+        </li>
+        <li>
+          <div class="image">
+          <img src="./images/location03/area01-03.jpg">
+          </div>
+          <div class="txt info">
+          <img src="./images/location03/area01-03-txt.svg">
+          </div>
+        </li>
+      </ul>
+      <ul class="column02">
+      <li>
+          <div class="image">
+          <img src="./images/location03/area01-04.jpg">
+          </div>
+          <div class="txt info">
+          <img src="./images/location03/area01-04-txt.svg">
+          </div>
+        </li>
+        <li>
+          <div class="image">
+          <img src="./images/location03/area01-05.jpg">
+          </div>
+          <div class="txt info">
+          <img src="./images/location03/area01-05-txt.svg">
+          </div>
+        </li>
+      </ul>
+    </div><!-- inner -->
+  </section><!-- areas area01 -->
+
+
+
+  <section class="sections areas area02 clearfix">
+    <div class="image img00">
+      <img src="./images/location03/area02-00.jpg">
+      <p class="annotation">image photo</p>
+    </div>
+    <div class="inner">
+      <p class="text">最寄りの2駅周辺に広がる商店街や、<br class="pc">暮らしを彩る魅力的なスポットの数々。</p>
+      <p class="lead">「下丸子」駅と「鵜の木」駅の周辺には、活気あふれる青果店やベーカリーなど新旧の魅力的な店舗が連なる商店街や、個性豊かなカフェなどが点在。つい立ち寄りたくなるスポットが、日々の暮らしを彩ります。</p>
+      <div class="box">
+        <ul class="column02">
+          <li>
+            <div class="image">
+            <img src="./images/location03/area02-01.jpg">
+            </div>
+            <div class="txt">
+            <img src="./images/location03/area02-01-txt.svg">
+            </div>
+          </li>
+          <li>
+            <div class="image">
+            <img src="./images/location03/area02-02.jpg">
+            </div>
+            <div class="txt">
+            <img src="./images/location03/area02-02-txt.svg">
+            </div>
+          </li>
+        </ul>
+        <div class="event">
+            <p class="lead">いくつもの商店街が身近にある「シティテラス下丸子」。下丸子駅前の「下丸子商栄会」では、地元の子どもたちも参加するパレードを開催。鵜の木駅前にある「鵜の木商店街」では、秋田名物なまはげが登場する「全国鵜の木まつり」を開催するなど、住民と一体となって地域を盛り上げるイベントも行なっています。</p>
+            <p class="mini">※イベントの開催内容は変更・中止になる場合があります。</p>
+            <div class="image">
+            <img src="./images/location03/area02-03.png">
+            </div>
+        </div><!-- event -->
+      </div><!-- box -->
+      </div><!-- inner -->
+  </section><!-- areas area02 -->
+
+
+
+  <section class="sections areas area03 clearfix">
+    <div class="inner">
+
+
+      </div><!-- inner -->
+  </section><!-- areas area03 -->
+
+
+
+  <section class="sections areas area04 clearfix">
+    <div class="inner">
+
+
+    </div><!-- inner -->
+  </section><!-- areas area04 -->
+
+
+
+<div class="annotation-area">
+<p class="page-annotation js-scroll wrap clearfix">※掲載の距離表示については地図上の概測距離を、徒歩分数表示については80mを1分として算出し、端数を切り上げたものです。<br>※掲載の環境写真は2023年7月に撮影。<br>※店舗・施設の内容等は将来変わる場合があります。<br>※掲載の情報は2023年7月現在のものです。</p></div>
+</main>
+
+  <div class="location-link location-link--3">
+    <a href="location01.html"><p>さまざまな都市機能に近接する「下丸子」</p></a>
+    <a href="location02.html"><p>豊かな自然の潤いを身近に</p></a>
+    <a href="location03.html" class="current"><p>毎日を支える充実の利便施設</p></a>
+  </div>
+
+<?php require(INCLUDES."footer.php"); ?>
+</body>
+</html>
