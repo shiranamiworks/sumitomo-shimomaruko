167ba5aec215f95bc8de45f37acaaaadf7060790
福井彩佳 <ayaka.fsp@gmail.com>
2025-01-17 17:19:06 +0900

    no message

--- develop/__NoDeploy__include/header.php
+++ develop/__NoDeploy__include/header.php
@@ -143,7 +143,6 @@ $folder_name = $site_setting["area"]."/".$site_setting["prop"]."/";
             <li><a href="map.html" target="_blank">現地案内図</a></li>
             <li><a href="/shuto/shimomaruko/faq.cgi" target="_blank">FAQ</a></li>
             <li><a href="javascript:outline();">物件概要</a></li>
-            <li><a href="javascript:blog();">マンション情報ブログ</a></li>
           </ul>
           <div class="header-sns">
             <a href="https://x.com/Cte_shimomaruko" target="_blank"><img src="./images/common/icn_x.png" alt="X"></a>
@@ -168,7 +167,6 @@ $folder_name = $site_setting["area"]."/".$site_setting["prop"]."/";
         <li><a href="<?= $page_settings["directory_pos"]  ?>access.html">アクセス</a></li>
         <li><a href="<?= $page_settings["directory_pos"]  ?>map.html" target="_blank">現地案内図</a></li>
         <li><a href="javascript:outline();">物件概要</a></li>
-        <li><a href="javascript:blog();">マンション情報ブログ</a></li>
         <li><a href="/shuto/shimomaruko/faq.cgi" target="_blank">FAQ</a></li>
         <script type="text/javascript" src="/bukken_manage/bukken_manage_entry_button.cgi?AREA_CD=shuto&PROP_CD=shimomaruko&LINK=sp-menu"></script>
       </ul>
